e110f7348ff28fc2cefefad07935f017db95e724
Francesco's MBP <franten94@gmail.com>
2023-02-12 14:34:41 -0400

    updated route

--- routes/clients.js
+++ routes/clients.js
@@ -3,71 +3,36 @@ const router = express.Router();
 const Client = require("../models/clients");
 const verifyJWT = require('../utils/verifyJWT');
 
-
 // /api/clients routes
 
-// all clients 
-router.get("", verifyJWT, async (req, res) => {
+const handleGetClients = async (req, res) => {
+    if (!req.user.esAdmin) return res.status(401).json({ message: "Auth Failed" });
     try {
-        if (req.user.esAdmin) {
-            const data = await Client.find();
-            res.status(200).json(data)
-        } else {
-            res.status(401).json({ message: "Auth Failed" })
-        }
+        const data = await Client.find();
+        return res.status(200).json(data);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
     }
-    catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-})
+};
 
-// new client
-router.post("", async (req, res) => {
+const handleCreateClient = async (req, res) => {
     try {
-        const data = new Client({
-            nombre: req.body.nombre,
-            apellido: req.body.apellido,
-            email: req.body.email,
-            telefono: req.body.telefono,
-            latitud: req.body.latitud,
-            longitud: req.body.longitud,
-            tipoDeVivienda: req.body.tipoDeVivienda,
-            tipoDeServicio: req.body.tipoDeServicio,
-            tipoDeTecnologia: req.body.tipoDeTecnologia,
-            planActual: req.body.planActual,
-            companiaActual: req.body.companiaActual,
-            atributos: req.body.atributos,
-            pagaria: req.body.pagaria,
-            rif: req.body.rif,
-            pagariaInstalacion: req.body.pagariaInstalacion,
-            nombreDeVivienda: req.body.nombreDeVivienda,
-            numeroDeVivienda: req.body.numeroDeVivienda,
-            rangoMinimoDePago: req.body.rangoMinimoDePago,
-            rangoMaximoDePago: req.body.rangoMaximoDePago,
-            esPotencial: req.body.esPotencial,
-            yaTieneServicio: req.body.yaTieneServicio,
-            calificacion: req.body.calificacion,
-            calificacionCalidadPrecio: req.body.calificacionCalidadPrecio,
-            loRemplazaria: req.body.loRemplazaria,
-            loAdquiriria: req.body.loAdquiriria,
-            loRecomendaria: req.body.loRecomendaria,
-            vendedor: req.body.vendedor
-        })
+        const data = new Client(req.body);
         const dataToSave = await data.save();
-        res.status(200).json(dataToSave)
-    }
-    catch (error) {
+        return res.status(200).json(dataToSave);
+    } catch (error) {
         if (error.name === "ValidationError") {
             let errors = {};
-
             Object.keys(error.errors).forEach((key) => {
                 errors[key] = error.errors[key].message;
             });
-            console.log(errors)
             return res.status(400).send(errors);
         }
-        res.status(500).json({ message: error.message })
+        return res.status(500).json({ message: error.message });
     }
-})
+};
+
+router.get("", verifyJWT, handleGetClients);
+router.post("", handleCreateClient);
 
 module.exports = router;
